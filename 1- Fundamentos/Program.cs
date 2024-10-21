bf7c541c388251666861306826d573e028e5f3e4
Marcos G. Júnior <markos-jr@outlook.com>
2024-10-20 22:17:11 -0300

    Add project files.

---
+++ 1- Fundamentos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Fundamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeira aula no Visual Studio!");
+
+            Console.WriteLine("Tecle enter para finalizar");
+
+            Console.ReadLine(); //Espera uma ação do usuário
+
+        }
+    }
+}
